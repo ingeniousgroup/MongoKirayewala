1688e6bfd7f998a6daec6e9cb76b9a86176536cb
Mausam <techdevelopers@gamil.com>
2023-05-16 23:19:18 -0700

    Mausam

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,8 +1,18 @@
+import email from "../Authentication/email.js";
 import { User } from "../model/user.model.js";
 export const userCheck = async (request,response,next)=>{
     try{
+        console.log(request.body);
         const data = await User.findOne({email : request.body.email});
-        console.log(data);
+        if(!data?.email)
+            {
+                console.log('Yaha pe aa gya...');
+                if(request.body.status)
+                {
+                    console.log('Yaha pe aa gya...2');
+                    email(request.body.email,"User verification from Kirayewala",request.body.name,request.body.otp);
+                }
+            }
         data?.email ?  response.status(200).json({user:data,status : false}) : response.status(200).json({message : 'You can register now...',status : true});
     }   
     catch(err)
