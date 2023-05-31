120aba160f9ac28bac11de7322976384f66e083f
vijay <vishwakarma.vijay.1001@gmail.com>
2023-05-31 13:56:43 +0530

    ugfd

--- route/owner.route.js
+++ route/owner.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import multer from "multer";
 const upload = multer({dest:'public/upload'});
-import { signIn,signUp ,viewProperty ,updateProperty,houseRequestFromTenant, addPropertyDetails, owner_change_password,addProperty, owner_view_profile, viewEnquiry,subscription ,expireSubscription, viewPropertyById, removePropertyById, showSubscriptions, removePropertyDetailsById, removeTenantRequest, updateBalance, findAdmin, houseRequestFromTenantWithoutAdmin, findPropertyByFurnishing} from "../controller/owner.controller.js";
+import { signIn,signUp ,viewProperty ,updateProperty,houseRequestFromTenant, addPropertyDetails, owner_change_password,addProperty, owner_view_profile, viewEnquiry,subscription ,expireSubscription, viewPropertyById, removePropertyById, showSubscriptions, removePropertyDetailsById, removeTenantRequest, updateBalance, findAdmin, houseRequestFromTenantWithoutAdmin, findPropertyByFurnishing, requestUpdate} from "../controller/owner.controller.js";
 import { body } from "express-validator";
 
 
@@ -61,4 +61,6 @@ router.post("/viewAdmin",findAdmin);
 router.post("/request",houseRequestFromTenantWithoutAdmin);
 
 router.post("/findPropertyByCategory",findPropertyByFurnishing);
+
+router.post("/updateRequest",requestUpdate);
 export default router;
